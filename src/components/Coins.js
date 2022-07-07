baef59519d4477e85a5050c4e33168acc0b2db27
DeepCoder28 <agrawaldeepak118@gmail.com>
2022-07-07 23:36:31 +0530

    Final Commit

---
+++ src/components/Coins.js
@@ -0,0 +1,32 @@
+import React from "react";
+import CoinItem from "./CoinItem";
+import "./Coins.css";
+import Coin from "../routes/Coin";
+import { Link } from "react-router-dom";
+
+const Coins = ({ coins }) => {
+  return (
+    <div className="container">
+      <div>
+        <div className="heading">
+          <p>#</p>
+          <p className="coin-name">Coin</p>
+          <p>Price</p>
+          <p>24h</p>
+          <p className="hide-mobile">Volume</p>
+          <p className="hide-mobile">Mkt Capt</p>
+        </div>
+
+        {coins.map((coin) => {
+          return (
+            <Link to={`/coin/${coin.id}`} element={<Coin />} key={coin.id}>
+              <CoinItem coin={coin} />
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Coins;
